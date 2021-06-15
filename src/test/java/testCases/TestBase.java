8e57981b4ae7d3072af9e006eeaedfb5c026f38c
Mohamed Essam <mohamed.essam@softxpert.com>
2021-06-15 22:19:15 +0200

    fix chrome issue

--- src/test/java/testCases/TestBase.java
+++ src/test/java/testCases/TestBase.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,44 +24,20 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase {
     public static WebDriver driver ;
-
-    public static String downloadPath = System.getProperty("user.dir") + "\\Downloads";
-
     public static String url = "https://operator.postscanmail.com/" ;
-
-
-    public static FirefoxOptions firefoxOption() {
-        FirefoxOptions option = new FirefoxOptions();
-        option.addPreference("browser.download.folderList", 2);
-        option.addPreference("browser.download.dir", downloadPath);
-        option.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf");
-        option.addPreference("browser.download.manager.showWhenStarting", false);
-        option.addPreference("pdfjs.disabled", true);
-        return option;
-    }
-
-    public static ChromeOptions chromeOption() {
-        ChromeOptions options = new ChromeOptions();
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default.content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", downloadPath);
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        return options;
-    }
 
     @BeforeClass
     @Parameters({"browser"})
     public void startDriver(@Optional("chrome") String browserName) throws IOException {
 
         if (browserName.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
-            driver = new ChromeDriver(chromeOption());
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+            driver = new ChromeDriver();
         }
 
         else if(browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
-            driver = new FirefoxDriver(firefoxOption());
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\drivers\\geckodriver.exe");
+            driver = new FirefoxDriver();
         }
 
         driver.manage().window().maximize();
